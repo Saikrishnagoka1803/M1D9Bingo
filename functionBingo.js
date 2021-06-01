d6ccc393e9440ac21172fd81c519b043e94e7618
Saikrishnagoka1803 <84295290+Saikrishnagoka1803@users.noreply.github.com>
2021-06-01 15:33:13 +0100

    Update functionBingo.js

--- functionBingo.js
+++ functionBingo.js
@@ -1,6 +1,7 @@
 window.onload = function()
 {
     displayAllNumbers()
+    
 }
 
 
@@ -8,12 +9,24 @@ window.onload = function()
 
 const displayAllNumbers = function()
 {
-let numbersBox = document.getElementById('numbers_box')
+let numbers_container = document.getElementById('numbers_box')
     for(i=1; i<=76; i++)
     {
         let newNumber = document.createElement('div')
         newNumber.innerText = i
         newNumber.classList.add('number')
-        numbersBox.appendChild(newNumber)
+        numbers_container.appendChild(newNumber)
     }
+}
+const generateRandomNum = function()
+{
+    let elements = []
+    let generatedRnNum = Math.floor(Math.random()*76)
+    for(let i=0;i<=75; i++)
+    {
+    let check = document.getElementsByClassName('number')[i].innerHTML
+        if(generatedRnNum == check)
+        document.getElementsByClassName('number')[i].classList.add('highlight')
+    }
+    //elements.push(document.getElementsByClassName('highlight'))
 }
